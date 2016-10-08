bd30502a83a3d0e4242412dcf2eb4b28a4a83687
Denyce Giron Loredo <deny_giron@yahoo.com>
2016-10-08 10:15:30 -0400

    Primer commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+
+This is simple Java program
+Call this file HelloWorld.java.
+*/
+
+class HelloWorld
+{
+
+// A Java program begins with a call to main ().
+   public static void main (String args[])
+   {
+     System.out.println("Hello from Java!!");
+   }
+}
